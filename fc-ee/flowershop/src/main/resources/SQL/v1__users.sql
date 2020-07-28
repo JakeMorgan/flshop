288ccfd71e12ff6520c2c1dfaf2fab0b6c07a7a5
alexey.tarasenkov <uran-2010@mail.ru>
2020-07-28 16:46:20 +0300

    Add Discount

--- fc-ee/flowershop/src/main/resources/SQL/v1__users.sql
+++ fc-ee/flowershop/src/main/resources/SQL/v1__users.sql
@@ -4,6 +4,7 @@ UserName nvarchar(30),
 Password nvarchar(30),
 Address nvarchar(30),
 Phone nvarchar(20),
-Balance Decimal);
+Balance Decimal,
+Discount int);
 
-INSERT into Users values(0, 'Admin', 'Admin', 'Plot', '89205152432', 5000);
+INSERT into Users values(0, 'Admin', 'Admin', 'Plot', '89205152432', 5000, 0);
